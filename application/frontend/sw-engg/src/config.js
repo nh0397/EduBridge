03396331513827beeeb888ed7a253822e5bce3c2
nh0397 <naisarg.halvadiya@gmail.com>
2024-03-22 22:55:33 -0700

    Milestone documentation added

--- application/frontend/sw-engg/src/config.js
+++ application/frontend/sw-engg/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    BASE_URL: 'http://localhost:3001', // Change this to your actual API domain
+    BASE_URL: 'http://54.82.98.216:3001', // Change this to your actual API domain
   };
   
   export default config;
