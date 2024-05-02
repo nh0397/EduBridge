522aa037f5cb560b7770855d11cf0eaabadcaed2
Pankuri Khare <pankurikhare@Pankuris-MacBook-Pro.local>
2024-05-02 10:12:23 -0700

    Removed unnecessary CSS imports

--- application/frontend/sw-engg/src/index.js
+++ application/frontend/sw-engg/src/index.js
@@ -4,11 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ThemeProvider} from "@mui/material";
 import theme from './/theme.jsx'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
