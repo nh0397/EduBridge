69d53937aacaef04db44bb782ba8f840331f8a55
Pankuri Khare <pankurikhare@sfs-wifi-dhcp-10-143-20-173.wifi.sfsu.edu>
2024-05-04 10:46:58 -0700

    Admin Page Background fixed

--- application/frontend/sw-engg/src/components/Landing/Admin/AdminPage.jsx
+++ application/frontend/sw-engg/src/components/Landing/Admin/AdminPage.jsx
@@ -26,33 +26,23 @@ const AdminPage = () => {
   };
 
   const handleRoleChange = async (email, newRole) => {
-    // Prevent changing the role of admins
     if (users.find(user => user.email === email && user.role === 'admin')) {
       setFeedback({ open: true, message: 'Admin role cannot be changed.', severity: 'error' });
       return;
     }
-
-    // Change role and show feedback
-      const updatedUsers = users.map(user =>
-        user.email === email ? { ...user, role: newRole } : user
-      );
-      try {
+    try {
       const response = await apiService.updateUserRole(email, newRole);
-      // Assuming response indicates success, then update local state
       if (response.success) {
-        const updatedUsers = users.map(user =>
+        setUsers(users.map(user =>
           user.email === email ? { ...user, role: newRole } : user
-        );
-        setUsers(updatedUsers);
+        ));
+        setFeedback({ open: true, message: 'Role updated successfully.', severity: 'success' });
       } else {
         console.error('Failed to update user role in the database.');
       }
     } catch (error) {
       console.error('Error updating user role:', error);
     }
-    loadUsers()
-    setUsers(updatedUsers);
-    setFeedback({ open: true, message: 'Role updated successfully.', severity: 'success' });
   };
 
   const handleCloseFeedback = () => {
@@ -66,80 +56,81 @@ const AdminPage = () => {
   );
 
   return (
-      <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            padding: theme.spacing(3),
-            height: 'auto',
-            backgroundImage: theme.backgroundImage,
-            backgroundSize: theme.backgroundSize,
-            backgroundPosition: theme.backgroundPosition,
-            backgroundAttachment: theme.backgroundAttachment,
-            backgroundColor: theme.palette.background.default,
-          }}
-      >
-        <Paper elevation={6} square sx={{
-          width: '100%',
-          maxWidth: 800,
-          margin: theme.spacing(2),
-          padding: theme.spacing(3),
-          borderRadius: theme.shape.borderRadius,
-          backgroundColor: theme.palette.background.paper
-        }}>
-          <Typography variant="h4" gutterBottom sx={{ textAlign: 'center', marginBottom: theme.spacing(2) }}>
-            Admin Page
-          </Typography>
-          <TextField
-              fullWidth
-              variant="outlined"
-              label="Search users..."
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              sx={{ marginBottom: theme.spacing(2) }}
-          />
-          <Table sx={{ marginBottom: theme.spacing(2) }}>
-            <TableHead>
-              <TableRow>
-                <TableCell>First Name</TableCell>
-                <TableCell>Last Name</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>Role</TableCell>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: 'auto',
+        backgroundImage: 'url("../../../images/Background2.webp")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        minHeight: '100vh',
+        margin: 0,
+        padding: theme.spacing(3),
+      }}
+    >
+      <Paper elevation={6} square sx={{
+        width: '100%',
+        maxWidth: 800,
+        margin: theme.spacing(2),
+        padding: theme.spacing(3),
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: theme.palette.background.paper,
+      }}>
+        <Typography variant="h4" gutterBottom sx={{ textAlign: 'center', marginBottom: theme.spacing(2) }}>
+          Admin Page
+        </Typography>
+        <TextField
+          fullWidth
+          variant="outlined"
+          label="Search users..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          sx={{ marginBottom: theme.spacing(2) }}
+        />
+        <Table sx={{ marginBottom: theme.spacing(2) }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Role</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredUsers.map(user => (
+              <TableRow key={user.id}>
+                <TableCell>{user.first_name}</TableCell>
+                <TableCell>{user.last_name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  {user.role === 'admin' ? (
+                    'Admin'
+                  ) : (
+                    <Select
+                      value={user.role}
+                      onChange={(e) => handleRoleChange(user.email, e.target.value)}
+                      fullWidth
+                    >
+                      <MenuItem value="Student">Student</MenuItem>
+                      <MenuItem value="Instructor">Instructor</MenuItem>
+                    </Select>
+                  )}
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredUsers.map(user => (
-                  <TableRow key={user.id}>
-                    <TableCell>{user.first_name}</TableCell>
-                    <TableCell>{user.last_name}</TableCell>
-                    <TableCell>{user.email}</TableCell>
-                    <TableCell>
-                      {user.role === 'admin' ? (
-                          'Admin' // Display text only for admins
-                      ) : (
-                          <Select
-                              value={user.role}
-                              onChange={(e) => handleRoleChange(user.email, e.target.value)}
-                              fullWidth
-                              sx={{ textAlign: 'left' }}
-                          >
-                            <MenuItem value="Student">Student</MenuItem>
-                            <MenuItem value="Instructor">Instructor</MenuItem>
-                          </Select>
-                      )}
-                    </TableCell>
-                  </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Paper>
-        <Snackbar open={feedback.open} autoHideDuration={6000} onClose={handleCloseFeedback}>
-          <Alert onClose={handleCloseFeedback} severity={feedback.severity} sx={{ width: '100%' }}>
-            {feedback.message}
-          </Alert>
-        </Snackbar>
-      </Box>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+      <Snackbar open={feedback.open} autoHideDuration={6000} onClose={handleCloseFeedback}>
+        <Alert onClose={handleCloseFeedback} severity={feedback.severity} sx={{ width: '100%' }}>
+          {feedback.message}
+        </Alert>
+      </Snackbar>
+    </Box>
   );
 };
 
