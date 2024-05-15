4fa48cfe9d8854982d8b5dc4ee766306c85ff79d
Pankuri Khare <pankurikhare@sfs-wifi-dhcp-10-143-20-173.wifi.sfsu.edu>
2024-05-15 13:50:03 -0700

    Footer typo fixed and admin Navbar fixed

--- application/frontend/sw-engg/src/components/Navbar/Navbar.jsx
+++ application/frontend/sw-engg/src/components/Navbar/Navbar.jsx
@@ -28,7 +28,6 @@ function Navbar(props) {
             navigate(`/search-results?search=${encodeURIComponent(searchTerm)}`); // Correctly navigate to search results page
         }
     }
-    
 
     const handleLogout = () => {
         sessionStorage.clear();
@@ -58,6 +57,7 @@ function Navbar(props) {
 		props.toggleModal()
         props.modalTypeFunc('File-Upload')
     };
+
     const buttonStyle = {
         fontWeight: 'bold',
         borderRadius: '50px',
@@ -92,7 +92,6 @@ function Navbar(props) {
         fetchCoursesData();
     }, []);
 
-
     const defaultMenu = [<DropdownMenuItem key="none">No Courses Available</DropdownMenuItem>];
     const buildMenuItems = (courses) => {
         return courses.map(course => {
@@ -110,7 +109,6 @@ function Navbar(props) {
 
     const courseMenuItems = buildMenuItems(courses);
 
-
     return (
         <Box sx={{
             display: 'flex',
@@ -125,7 +123,6 @@ function Navbar(props) {
             backgroundPosition: 'center',
             marginLeft: 'auto',
             marginRight: 'auto',
-    
         }}>
             <Link to={homePath} style={{
                 textDecoration: 'none',
@@ -138,127 +135,134 @@ function Navbar(props) {
                     <img src={logo} alt="App Logo" style={{ width: '100%' }} />
                 </Avatar>
             </Link>
-            <form onSubmit={handleSearchSubmit}>
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <TextField
-                variant="outlined"
-                size="small"
-                placeholder="Search Files"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                    sx={{
-                        borderRadius: '20px',
-                        backgroundColor: 'white',
-                        width: '500px',
-                        flexGrow: 1,
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': { border: 'none' },
-                            '&:hover fieldset': { border: 'none' },
-                            '&.Mui-focused fieldset': { border: 'none' },
-                        }
-                    }}
-                    InputProps={{
-                        endAdornment: searchTerm && (
-                            <InputAdornment position="end">
-                                <IconButton
-                                    onClick={() => setSearchTerm('')}
-                                    edge="end"
-                                    sx={{ size:"0x", color: '#005B4B' }}
-                                >
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </IconButton>
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-
-        <Button type="submit" color="inherit" sx={{
-            p: 0, 
-            minWidth: 'auto', 
-            backgroundColor: 'transparent', 
-            ml: 1,
-            '&:hover': { backgroundColor: 'transparent'}, // Ensure no background change on hover
-            '&:focus': { outline: 'none' } // Remove focus outline
-        }}>
-            <FontAwesomeIcon icon={faSearch} size="lg" style={{ color: '#005B4B' }} />
-        </Button>
-    </Box>
-</form>
-
+            
+            {userRole?.toLowerCase() !== 'admin' && (
+                <form onSubmit={handleSearchSubmit}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <TextField
+                            variant="outlined"
+                            size="small"
+                            placeholder="Search Files"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            sx={{
+                                borderRadius: '20px',
+                                backgroundColor: 'white',
+                                width: '500px',
+                                flexGrow: 1,
+                                '& .MuiOutlinedInput-root': {
+                                    '& fieldset': { border: 'none' },
+                                    '&:hover fieldset': { border: 'none' },
+                                    '&.Mui-focused fieldset': { border: 'none' },
+                                }
+                            }}
+                            InputProps={{
+                                endAdornment: searchTerm && (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            onClick={() => setSearchTerm('')}
+                                            edge="end"
+                                            sx={{ size:"0x", color: '#005B4B' }}
+                                        >
+                                            <FontAwesomeIcon icon={faTimes} />
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <Button type="submit" color="inherit" sx={{
+                            p: 0, 
+                            minWidth: 'auto', 
+                            backgroundColor: 'transparent', 
+                            ml: 1,
+                            '&:hover': { backgroundColor: 'transparent'}, // Ensure no background change on hover
+                            '&:focus': { outline: 'none' } // Remove focus outline
+                        }}>
+                            <FontAwesomeIcon icon={faSearch} size="lg" style={{ color: '#005B4B' }} />
+                        </Button>
+                    </Box>
+                </form>
+            )}
+            
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 {userRole?.toLowerCase() === 'instructor' && (
                     <Button onClick={openFileUploadModal} sx={buttonStyle}>
-                    <FontAwesomeIcon icon={faCloudUploadAlt} size="sm" />
-                    <Typography variant="body1">Upload New Content</Typography>
-                </Button>
+                        <FontAwesomeIcon icon={faCloudUploadAlt} size="sm" />
+                        <Typography variant="body1">Upload New Content</Typography>
+                    </Button>
                 )}
-                <Button onClick={openDiscussionModal} sx={buttonStyle}>
-                    <FontAwesomeIcon icon={faPlus} size="sm" />
-                    <Typography variant="body1">Create Discussion</Typography>
-                </Button>
-                <Button onClick={() => null} sx={buttonStyle}>
-                    <FontAwesomeIcon icon={faBook} size="sm" />
-                    <Typography variant="body1">Courses</Typography>
-                </Button>
+                {userRole?.toLowerCase() !== 'admin' && (
+                    <>
+                        <Button onClick={openDiscussionModal} sx={buttonStyle}>
+                            <FontAwesomeIcon icon={faPlus} size="sm" />
+                            <Typography variant="body1">Create Discussion</Typography>
+                        </Button>
+                        <Button onClick={() => null} sx={buttonStyle}>
+                            <FontAwesomeIcon icon={faBook} size="sm" />
+                            <Typography variant="body1">Courses</Typography>
+                        </Button>
+                    </>
+                )}
                 <Button onClick={handleClick} sx={buttonStyle}>
                     <Avatar sx={{ bgcolor: theme.palette.primary.main, width: 50, height: 50, alignItems: 'right', marginLeft: -16, marginRight: -16 }}>
                         {getInitials(firstName, lastName)}
                     </Avatar>
                 </Button>
                 <Menu
-    className='menu-box'
-    id="account-menu"
-    anchorEl={anchorEl}
-    open={open}
-    onClose={handleClose}
-    MenuListProps={{
-        'aria-labelledby': 'account-button',
-    }}
-    PaperProps={{
-        style: {
-            width: '300px', // Increase the width as needed
-            padding: '20px',  // Optional: add some padding around the items
-			paddingBottom:'0px'
-        }
-    }}
->
-    <div className='name-box'>
-        <div>
-            <Avatar 
-                sx={{ 
-                    bgcolor: theme.palette.primary.main, 
-                    width: 60,  // Increased width
-                    height: 60, // Increased height
-                }}
-            >
-                {getInitials(firstName, lastName)}
-            </Avatar>
-        </div>
-        <div>
-            <div className='name-text'>
-            <div className='firstName'>{sessionStorage.getItem("firstName")}</div>
-            <div className='lastName'>{sessionStorage.getItem("lastName")}</div>
-        </div>
-        <div>
-            {sessionStorage.getItem('userEmail')}
-        </div>
-        <div>
-            {userRole && (
-    <div>
-        {userRole[0].toUpperCase() + userRole.substring(1)}
-    </div>
-)}
-        </div>
-        </div>
-    </div>
-    <MenuItem onClick={handleLogout} className='logout-text'>
-            <FontAwesomeIcon icon={faSignOutAlt} style={{ marginRight: '8px' }} />
-            Logout
-    </MenuItem>
-</Menu>
-
+                    className='menu-box'
+                    id="account-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                        'aria-labelledby': 'account-button',
+                    }}
+                    PaperProps={{
+                        style: {
+                            width: '300px', // Increase the width as needed
+                            padding: '20px',  // Optional: add some padding around the items
+                            paddingBottom: '0px'
+                        }
+                    }}
+                >
+                    <div className='name-box'>
+                        <div>
+                            <Avatar 
+                                sx={{ 
+                                    bgcolor: theme.palette.primary.main, 
+                                    width: 60,  // Increased width
+                                    height: 60, // Increased height
+                                }}
+                            >
+                                {getInitials(firstName, lastName)}
+                            </Avatar>
+                        </div>
+                        <div>
+                            <div className='name-text'>
+                                <div className='firstName'>{sessionStorage.getItem("firstName")}</div>
+                                <div className='lastName'>{sessionStorage.getItem("lastName")}</div>
+                            </div>
+                            <div>
+                                {sessionStorage.getItem('userEmail')}
+                            </div>
+                            <div>
+                                {userRole && (
+                                    <div>
+                                        {userRole[0].toUpperCase() + userRole.substring(1)}
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <MenuItem onClick={handleLogout} className='logout-text'>
+                        <FontAwesomeIcon icon={faSignOutAlt} style={{ marginRight: '8px' }} />
+                        Logout
+                    </MenuItem>
+                </Menu>
             </Box>
         </Box>
     );
 }
+
 export default Navbar;
+
