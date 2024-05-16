9714a74e553af97263fe048a99392223f24798d2
nh0397 <naisarg.halvadiya@gmail.com>
2024-05-15 23:57:24 -0700

    Config map changed for production

--- application/frontend/sw-engg/src/config.js
+++ application/frontend/sw-engg/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // BASE_URL: 'https://swe-syntaax-squuad.duckdns.org', // Change this to your actual API domain
-    BASE_URL: 'http://localhost:3001', // Change this to your actual API domain
+    BASE_URL: 'https://swe-syntaax-squuad.duckdns.org', // Change this to your actual API domain
+    // BASE_URL: 'http://localhost:3001', // Change this to your actual API domain
 
   };
   
