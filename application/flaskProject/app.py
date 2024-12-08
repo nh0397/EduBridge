29d76dea84314fbe8a763e08b819d50400ef34f0
Naisarg Halvadiya <naisarg.halvadiya@gmail.com>
2024-12-07 23:35:10 -0800

    CORS configuration in flask app

--- application/flaskProject/app.py
+++ application/flaskProject/app.py
@@ -3,8 +3,10 @@ import joblib
 import os
 import pandas as pd
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
